a74656ed731ccd9cafee81a0de5b21607c85adf6
MeowNeko <h.mizzyu@gmail.com>
2024-05-15 18:17:21 +0800

    Init database to controller

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/controller/categories_controller.dart';
 import 'package:todo_app/controller/tasks_controller.dart';
+import 'package:todo_app/core/databases/database_helper.dart';
 import 'package:todo_app/theme.dart';
 import 'package:todo_app/view/pages/edit_task_page.dart';
 import 'package:todo_app/view/pages/home_page.dart';
 import 'package:todo_app/view/pages/add_task_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final categories = await DatabaseHelper.getCategories();
+  final tasks = await DatabaseHelper.getTasks();
+
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (_) => CategoriesController(categoriesList: []),
+          create: (_) => CategoriesController(categoriesList: categories),
         ),
         ChangeNotifierProvider(
-          create: (_) => TasksController(taskList: []),
+          create: (_) => TasksController(taskList: tasks),
         ),
       ],
       child: const MyApp(),
